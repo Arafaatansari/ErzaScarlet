0934c3f8ca9482afd6c56cd201fe2feb8f6176e6
noobsohail <sikhoshikaay@gmail.com>
2021-08-27 18:29:03 +0530

    added

--- ErzaScarlet/__init__.py
+++ ErzaScarlet/__init__.py
@@ -89,6 +89,7 @@ if ENV:
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY")
     
     
     try:
