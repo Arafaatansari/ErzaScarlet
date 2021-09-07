c22136f979469037060b262e41b0116c6442ce5c
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <87354331+noobsohail@users.noreply.github.com>
2021-09-08 01:34:37 +0530

    Update chatbot.py

--- ErzaScarlet/modules/chatbot.py
+++ ErzaScarlet/modules/chatbot.py
@@ -139,8 +139,8 @@ __help__ = f"""
 Chatbot utilizes the CoffeeHouse API and allows mizuhara to talk and provides a more interactive group chat experience.
 *Commands:* 
 *Admins only:*
- ✪ `/addchat`*:* Enables Chatbot mode in the chat.
- ✪ `/rmchat`*:* Disables Chatbot mode in the chat.
+  `/addchat` *:* Enables Chatbot mode in the chat.
+  `/rmchat` *:* Disables Chatbot mode in the chat.
 Reports bugs at @{SUPPORT_CHAT}
 [Powered by CoffeeHouse](https://coffeehouse.intellivoid.net) from @Intellivoid
 """
