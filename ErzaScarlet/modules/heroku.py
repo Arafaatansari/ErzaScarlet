8f0ea420cd3d88418ea82cf72cc3ed10077ee003
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <sikhoshikaay@gmail.com>
2021-09-20 00:39:49 -0700

    Update heroku.py

--- ErzaScarlet/modules/heroku.py
+++ ErzaScarlet/modules/heroku.py
@@ -5,7 +5,7 @@ import os
 import heroku3
 import requests
 
-from ErzaScarlet import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID, DEV_USERS
+from ErzaScarlet import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
 from ErzaScarlet.events import register
 
 heroku_api = "https://api.heroku.com"
@@ -16,7 +16,7 @@ Heroku = heroku3.from_key(HEROKU_API_KEY)
 async def variable(var):
     if var.fwd_from:
         return
-    if var.sender_id == DEV_USERS:
+    if var.sender_id == OWNER_ID:
         pass
     else:
         return
@@ -105,7 +105,7 @@ async def variable(var):
 async def dyno_usage(dyno):
     if dyno.fwd_from:
         return
-    if dyno.sender_id == DEV_USERS:
+    if dyno.sender_id == OWNER_ID:
         pass
     else:
         return
@@ -172,7 +172,7 @@ async def dyno_usage(dyno):
 async def _(dyno):
     if dyno.fwd_from:
         return
-    if dyno.sender_id == DEV_USERS:
+    if dyno.sender_id == OWNER_ID:
         pass
     else:
         return
