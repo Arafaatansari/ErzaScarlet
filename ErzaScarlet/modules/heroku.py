ab6fe6b86e53ce8775fa4c0d2248484acd3d10a4
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <87354331+noobsohail@users.noreply.github.com>
2021-09-20 00:40:55 -0700

    Update heroku.py

--- ErzaScarlet/modules/heroku.py
+++ ErzaScarlet/modules/heroku.py
@@ -191,7 +191,7 @@ async def _(dyno):
         dyno.chat_id,
         "logs.txt",
         reply_to=dyno.id,
-        caption="DaisyX Bot Logz.",
+        caption="Erza Bot Logs.",
     )
 
     await asyncio.sleep(5)
