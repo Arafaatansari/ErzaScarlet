ad72e5ef04c9a59f86b0d5265441f2ba06bdc84c
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <sikhoshikaay@gmail.com>
2021-09-25 01:12:03 -0700

    Update global_bans.py

--- ErzaScarlet/modules/global_bans.py
+++ ErzaScarlet/modules/global_bans.py
@@ -497,13 +497,21 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
-**Admin only**:
- - /antispam <on/off/yes/no>: Check and/or change anti-spam settings.
- Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect
- you and your groups by removing spam flooders as quickly as possible.
- Note: Users can appeal gbans or report spammers at @ErzaScarlet_Justice
- Erza also integrates @Spamwatch , combot antispam and @Intellivoid's SpamProtectionBot API to remove Spammers as much as possible from your chatroom!
-"""
+Get information about anime, manga or characters from [AniList](anilist.co).
+*Available commands:*
+                               
+➩ *Admins only:*                            
+ ✪ /antispam <on/off/yes/no>*:* will toggle our antispam tech or return your current settings.
+
+ Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect you and your groups by removing spam flooders as quickly as possible.
+ *Note:* Users can appeal gbans or report spammers at @ErzaScarlet_Justice
+
+ This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
+ *What is SpamWatch?*
+ SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters.
+ Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
+ *Note:* Users can appeal spamwatch bans at @SpamwatchSupport                          
+ """
 
 GBAN_HANDLER = CommandHandler("gban", gban)
 UNGBAN_HANDLER = CommandHandler("ungban", ungban)
