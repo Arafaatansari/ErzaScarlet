c58c3a8b8b3ebc420342a1142de792a3d3def4f9
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <87354331+noobsohail@users.noreply.github.com>
2021-09-27 00:34:41 +0530

    Update songs.py

--- ErzaScarlet/modules/songs.py
+++ ErzaScarlet/modules/songs.py
@@ -14,7 +14,7 @@ from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio
 from telethon import events
 
-from ErzaScarlet import register
+from ErzaScarlet.events import register
 from ErzaScarlet.utils import progress
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
